4f228eecd4c8f9a5c1090db614c80b18914c9bda
advego98 <adrian.vego98@gmail.com>
2020-06-02 13:52:22 +0200

    error css secure

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -20,14 +20,14 @@
 
     <!-- Styles -->
     <link href="{{ secure_asset('css/app.css') }}" rel="stylesheet">
-    <link href="{{ URL::secure_asset('css/form.css') }}" rel="stylesheet">
-    <link href="{{ URL::secure_asset('css/headerfooter.css') }}" rel="stylesheet">
-    <link href="{{ URL::secure_asset('css/paymentmethod.css') }}" rel="stylesheet">
-    <link href="{{ URL::secure_asset('css/planchoose.css') }}" rel="stylesheet">
-    <link href="{{ URL::secure_asset('css/register.css') }}" rel="stylesheet">
-    <link href="{{ URL::secure_asset('css/restpasswd.css') }}" rel="stylesheet">
-    <link href="{{ URL::secure_asset('css/profile.css') }}" rel="stylesheet">
-    <link href="{{ URL::secure_asset('css/admin.css') }}" rel="stylesheet">
+    <link href="{{ secure_asset('css/form.css') }}" rel="stylesheet">
+    <link href="{{ secure_asset('css/headerfooter.css') }}" rel="stylesheet">
+    <link href="{{ secure_asset('css/paymentmethod.css') }}" rel="stylesheet">
+    <link href="{{ secure_asset('css/planchoose.css') }}" rel="stylesheet">
+    <link href="{{ secure_asset('css/register.css') }}" rel="stylesheet">
+    <link href="{{ secure_asset('css/restpasswd.css') }}" rel="stylesheet">
+    <link href="{{ secure_asset('css/profile.css') }}" rel="stylesheet">
+    <link href="{{ secure_asset('css/admin.css') }}" rel="stylesheet">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no" />
 
