fcb4b221d867b5fccbfec61154be055365103d05
advego98 <adrian.vego98@gmail.com>
2020-06-02 13:49:00 +0200

    errores css secure

--- resources/views/layouts/multimedia.blade.php
+++ resources/views/layouts/multimedia.blade.php
@@ -21,7 +21,7 @@
 
 
     <!-- Styles -->
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link href="{{ URL::secure_asset('css/app.css') }}" rel="stylesheet">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no" />
 
 </head>
