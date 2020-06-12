b55ff76fdb7326196c881eae585866e20767ebf8
advego98 <adrian.vego98@gmail.com>
2020-06-12 22:30:10 +0200

    password restablecer 2/2

--- resources/views/auth/passwords/email.blade.php
+++ resources/views/auth/passwords/email.blade.php
@@ -9,23 +9,23 @@
         <div class="password-body">
 
             <h1 class="titlepassword">¿Como restablecer la contraseña?</h1>
-            @if (session('status'))
-                <div class="alert alert-success" role="alert">
-                    {{ session('status') }}
-                </div>
-            @endif
 
             <form method="POST" action="{{ route('password.email') }}">
-                        @csrf
+                @csrf
 
 
-                        <div class="passwordsection">
-                            <label class="option">Correo</label>
+                <div class="passwordsection">
+                    @if (session('status'))
+                        <div class="alert_success" role="alert">
+                            {{ session('status') }}
+                        </div>
+                    @endif
+                    <label class="option">Correo</label>
                             <p class="textinfo">Se te enviará un correo con las instrucciones para restablecer la contraseña</p>
                             <input id="email" type="email" class="forminput @error('email') is-invalid @enderror"  name="email" value="{{ old('email') }}" placeholder="nombre@ejemplo.com" required autocomplete="email" autofocus>
                             @error('email')
-                            <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
+                            <span class="alert_error" role="alert">
+                                        <strong>Espere para volver a intentarlo</strong>
                                     </span>
                             @enderror
                             <button class="login-button">Enviar</button>
